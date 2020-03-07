beb059cfca089338c1656bf4d9ac93b466f9526f
samridhharshit <samridhharsh@gmail.com>
2020-03-07 15:55:09 +0530

    basic structure for adminpage and Productsale page prepared

--- admindashboard/src/Components/ProgressBar.jsx
+++ admindashboard/src/Components/ProgressBar.jsx
@@ -3,8 +3,14 @@ import React from "react";
 export default class ProgressBar extends React.Component {
     render() {
         return (
-            <div>
-                <p>progressBar</p>
+            <div className="row progressBar">
+                <div className="col-sm-2 progressBar-icons">
+                    <i className="fa fa-tachometer-alt progress-bar-icon fa-2x" />
+                    <i className="fa fa-cog progress-bar-icon" />
+                </div>
+                <div className="col-sm-10">
+                    <p>progressBar</p>
+                </div>
             </div>
         );
     }
